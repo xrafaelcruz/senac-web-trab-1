5bb8bf9830f60f231227f4e65c68c39f39cfd201
Rafael Cruz <xrafaelcruz@gmail.com>
2021-09-11 11:48:19 -0300

    added components AppBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-function App() {
-  return (
-    <div className="App">
+import React from 'react'
+import AppBar from './components/AppBar'
 
+const App = () => {
+  return (
+    <div className="container-fluid">
+      <AppBar />
     </div>
   );
 }
